6aa9b2de407cceb5b6b014b8d77b3a4887f527bf
Maki <alejoaskercat12@gmail.com>
2024-09-20 01:24:11 -0500

    Merge branch 'master' of https://github.com/maxtrosy/LAB_EDD2
    # Conflicts:
    #	build/built-jar.properties

--- src/PANTALLAS/pantallaPlantilla.java
+++ src/PANTALLAS/pantallaPlantilla.java
@@ -196,6 +196,7 @@ public class pantallaPlantilla extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(pantallaPlantilla.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
